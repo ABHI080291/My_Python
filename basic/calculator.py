2156aa34066e4241cd191d4300e4db8ce9111707
Abhishek Kumar <ABHIVG4784@GMAIL.COM>
2025-04-20 17:34:03 +0530

    Add files via upload

---
+++ basic/calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum_of_num = num1 + num2
+
+print("sum of 2 numbers: ",sum_of_num)
+
+diff_of_num = num1 - num2
+
+print("diff of 2 number: ",diff_of_num)
+
+product_of_num = num1 * num2
+
+print("product of 2 numbers: ",product_of_num)
+
+division_of_num = num1 / num2
+
+print("division of 2 number: ",division_of_num)
